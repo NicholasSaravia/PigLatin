bd446c6644d035ef551cebbe463937c0a19b7ae4
NicholasSaravia <Nick.Saravia@gmail.com>
2017-01-09 13:35:51 -0500

    Foundation code for PigLatin Project

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -10,6 +10,28 @@ namespace PigLatin
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("what is your favorie food?");
+            string favoriteFood = Console.ReadLine();
+
+            Console.WriteLine("what color is your house?");
+            string houseColor = Console.ReadLine();
+
+            Console.WriteLine("Becuase your favorite food is " + favoriteFood + " then of course your house would be the color " + houseColor);
+
+            Console.WriteLine("Now let me ask you this: ");
+            Console.Write("What is the first three digits of your area code?");
+            int areaCode = int.Parse(Console.ReadLine());
+
+
+
+            if (areaCode == 864)
+            {
+                Console.WriteLine("");
+
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
