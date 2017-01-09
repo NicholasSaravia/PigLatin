cf29985e74316d0af2bd6e9df4cf6bf1b149de50
NicholasSaravia <Nick.Saravia@gmail.com>
2017-01-09 15:58:03 -0500

    A little more char and bug fix

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -10,24 +10,52 @@ namespace PigLatin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("what is your favorie food?");
-            string favoriteFood = Console.ReadLine();
+            Console.WriteLine("Do you like Cats or Dogs?");
+            string favoriteAnimal = Console.ReadLine();
 
             Console.WriteLine("what color is your house?");
             string houseColor = Console.ReadLine();
 
-            Console.WriteLine("Becuase your favorite food is " + favoriteFood + " then of course your house would be the color " + houseColor);
+            Console.WriteLine("Becuase you have a love for " +
+                              favoriteAnimal +
+                              " then of course your house would be the color "
+                              + houseColor +
+                              "!");
 
-            Console.WriteLine("Now let me ask you this: ");
-            Console.Write("What is the first three digits of your area code?");
+            Console.WriteLine("Now, time for a test:");
+            Console.Write("What is Greenville's three digit area code? ");
             int areaCode = int.Parse(Console.ReadLine());
-
-
 
             if (areaCode == 864)
             {
-                Console.WriteLine("");
+                if (favoriteAnimal == "Cats")
+                {
+                    char firstLetter = favoriteAnimal[0];
+                    char secondLetter = favoriteAnimal[1];
+                    char thirdLetter = favoriteAnimal[2];
+                    char fourthLetter = favoriteAnimal[3];
 
+                    Console.WriteLine($"Im so glad you like {secondLetter}{thirdLetter}{fourthLetter}{firstLetter}ay!");
+                }
+
+                else if (favoriteAnimal == "Dogs")
+                {
+                    char firstLetter = favoriteAnimal[0];
+                    char secondLetter = favoriteAnimal[1];
+                    char thirdLetter = favoriteAnimal[2];
+                    char fourthLetter = favoriteAnimal[3];
+
+                    Console.WriteLine("" + $"{secondLetter}{thirdLetter}{fourthLetter}{firstLetter}ay, are way more hyper than cats");
+                }
+
+                else
+                {
+                    Console.WriteLine("you did not answer the first question orrectlycay!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("That is not GVL's area code");
             }
 
             Console.ReadLine();
